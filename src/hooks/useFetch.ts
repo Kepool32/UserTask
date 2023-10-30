85826fdcb98ca322ce4caab90bcd130c2631f5c9
Kepool32 <kukso.artem@mail.ru>
2023-10-30 21:51:12 +0300

    Added optimization

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,23 +1,10 @@
 import { useEffect } from "react";
-import axios from "axios";
+
 import userStore from "store/UserStore";
 
 const useFetch = () => {
   useEffect(() => {
-    userStore.setLoading(true);
-
-    axios
-      .get("http://127.0.0.1:3001/")
-      .then((response) => {
-        const user = response.data;
-        userStore.setUser(user);
-      })
-      .catch((error) => {
-        console.error("Ошибка при получении данных:", error);
-      })
-      .finally(() => {
-        userStore.setLoading(false);
-      });
+    userStore.fetchData();
   }, []);
 };
 
